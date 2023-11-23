8673511d780d5ae1bdec8937c78495e0c3f84221
Malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2023-11-23 08:36:09 -0500

    Correction grc admin

--- NTPMBnew/grc/admin.py
+++ NTPMBnew/grc/admin.py
@@ -5,7 +5,7 @@ from .models import Personnegrc
 class PersonnegrcAdmin(admin.ModelAdmin):
     admin.site.site_header = 'Personnes GRC'
     list_display = ('codeGRC', 'dateprint', 'ferme','RA')
-    list_filter = ['province', 'ferme', 'RA', 'codeGRC']
+    list_filter = ['codeGRC', 'ferme', 'RA', ]
 
     def save_model(self, request, obj, form, change):
         obj.save()
