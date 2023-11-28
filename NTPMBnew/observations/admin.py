ebaff0d6c035864aa62b1cf151eeff223f1ff9a3
Malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2023-11-28 15:24:38 -0500

    Ajout de l'Admin pour gérer et compter les dossiers

--- NTPMBnew/observations/admin.py
+++ NTPMBnew/observations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import MBpersonnes
 # Register your models here.
+
+
+class PersonnesAdmin(admin.ModelAdmin):
+    model = MBpersonnes
+    list_display = ('code', 'completed', 'filecode', 'assistant')
+    can_delete = False
+
+
+admin.site.register(MBpersonnes, PersonnesAdmin)
